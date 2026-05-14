ae481b52267bbf1044005d84aeebe2f5e8aae1fe
a.dernovskiy <dernovski@icloud.com>
2026-05-14 10:28:42 +0700

    add comment repository

---
+++ src/main/java/org/example/repository/CommentRepository.java
@@ -0,0 +1,75 @@
+package org.example.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.CommentRequest;
+import org.example.model.Comment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+    private static final String COL_ID =      "id";
+    private static final String COL_TEXT =    "text";
+    private static final String COL_POST_ID = "post_id";
+
+    private static final RowMapper<Comment> COMMENT_ROW_MAPPER = (rs, rowNum) -> new Comment(
+            rs.getLong(COL_ID),
+            rs.getString(COL_TEXT),
+            rs.getLong(COL_POST_ID)
+    );
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Comment> findAllByPostId(long postId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM comments WHERE post_id = ?",
+                COMMENT_ROW_MAPPER,
+                postId
+        );
+    }
+
+    public Comment findById(long id) {
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM comments WHERE id = ?",
+                COMMENT_ROW_MAPPER,
+                id
+        );
+    }
+
+    public Comment save(Comment comment) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "INSERT INTO comments (text, post_id) VALUES (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            ps.setString(1, comment.text());
+            ps.setLong(2, comment.postId());
+            return ps;
+        }, keyHolder);
+        return findById(keyHolder.getKey().longValue());
+    }
+
+    public Comment update(long id, CommentRequest request) {
+        jdbcTemplate.update(
+                "UPDATE comments SET text = ? WHERE id = ?",
+                request.text(),
+                id
+        );
+        return findById(id);
+    }
+
+    public void deleteById(long id) {
+        jdbcTemplate.update(
+                "DELETE FROM comments WHERE id = ?", id
+        );
+    }
+}
