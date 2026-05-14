17a0e739ac827c5d6c3b2b99d46cb49317c21454
a.dernovskiy <dernovski@icloud.com>
2026-05-14 10:28:34 +0700

    add comment service

---
+++ src/main/java/org/example/service/CommentService.java
@@ -0,0 +1,44 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.CommentDto;
+import org.example.dto.CommentRequest;
+import org.example.mapper.CommentMapper;
+import org.example.model.Comment;
+import org.example.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentMapper commentMapper;
+
+    public List<CommentDto> getAllByPostId(long postId) {
+        return commentRepository.findAllByPostId(postId)
+                .stream()
+                .map(commentMapper::toDto)
+                .toList();
+    }
+
+    public CommentDto getById(long id) {
+        return commentMapper.toDto(commentRepository.findById(id));
+    }
+
+    public CommentDto create(CommentRequest request) {
+        Comment comment = new Comment(0L, request.text(), request.postId());
+        return commentMapper.toDto(commentRepository.save(comment));
+    }
+
+    public CommentDto update(long id, CommentRequest request) {
+        return commentMapper.toDto(commentRepository.update(id, request));
+    }
+
+    public void delete(long id) {
+        commentRepository.deleteById(id);
+    }
+
+}
