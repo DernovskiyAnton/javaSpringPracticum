c532a657aedd5fcd32e060f6dfa97b80fdae5f13
a.dernovskiy <dernovski@icloud.com>
2026-06-16 23:15:25 +0700

    fix magic numbers

--- src/main/java/org/example/config/WebAppInitializer.java
+++ src/main/java/org/example/config/WebAppInitializer.java
@@ -5,6 +5,13 @@ import jakarta.servlet.ServletRegistration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+public static final String UPLOAD_TEMP_DIR = "\"/tmp\"";
+    public static final long MAX_FILE_SIZE = 10 * 1024 * 1024;
+    public static final long MAX_REQUEST_SIZE = 20 * 1024 * 1024;
+    public static final int FILE_SIZE_Threshold = 0;
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[] {AppConfig.class};
@@ -23,10 +30,9 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
         registration.setMultipartConfig(
-                new MultipartConfigElement("/tmp",
-                        10 * 1024 * 1024,
-                        20 * 1024 * 1024,
-                        0)
+                new MultipartConfigElement(UPLOAD_TEMP_DIR,
+                        MAX_FILE_SIZE, MAX_REQUEST_SIZE,
+                        FILE_SIZE_Threshold)
         );
     }
 }
